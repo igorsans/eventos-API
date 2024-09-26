66fbe7eb68e6cdb516670820353c432c474e41a5
Igor Santos <igor.santos0@hotmail.com>
2024-09-26 17:58:51 -0300

    feat: event uppcoming

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,13 +2,13 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.domain.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -17,9 +17,16 @@ public class EventController {
     private EventService eventService;
 
     @PostMapping
-    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body ){
+    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body ) {
         Event newEvent = this.eventService.creatEvent(body);
         return ResponseEntity.ok(newEvent);
 
+    }
+
+
+@GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
 }
--- src/main/java/com/eventostec/api/domain/service/EventService.java
+++ src/main/java/com/eventostec/api/domain/service/EventService.java
@@ -3,9 +3,13 @@ package com.eventostec.api.domain.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.domain.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +18,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -46,6 +51,24 @@ public class EventService {
         return newEvent;
 
     }
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                "",
+                "",
+                event.getRemote(),
+                event.getEventUrl()
+//                implementar com AWS
+//                event.getImgUrl()
+        )).stream().toList();
+    }
+
+
+    //implementar com AWS
     private String uploadImg(MultipartFile multipartFile){
         String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
         try {
